a0e56e329ac409c2590057391c5385ec8f7f5b7e
Albert Reis <albertlreis@gmail.com>
2025-06-21 00:30:26 -0300

    Retornar categoria

--- app/Services/ProdutoService.php
+++ app/Services/ProdutoService.php
@@ -100,7 +100,14 @@ class ProdutoService
 
     public function listarProdutosFiltrados($request): LengthAwarePaginator
     {
-        $query = Produto::with(['variacoes.atributos', 'variacoes.estoque', 'variacoes.outlet', 'variacoes.outlets', 'imagemPrincipal']);
+        $query = Produto::with([
+            'categoria',
+            'variacoes.atributos',
+            'variacoes.estoque',
+            'variacoes.outlet',
+            'variacoes.outlets',
+            'imagemPrincipal'
+        ]);
 
         if (!empty($request->nome)) {
             $query->where('nome', 'ILIKE', '%' . $request->nome . '%');

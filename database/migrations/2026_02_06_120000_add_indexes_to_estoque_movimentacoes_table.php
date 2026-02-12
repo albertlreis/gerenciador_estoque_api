b0dd72f4222e793a957ed9b4a54167fcdd8bcbab
Albert Reis <albertlreis@gmail.com>
2026-02-12 09:09:00 -0300

    Desativar verificação de chave estrangeira

--- database/migrations/2026_02_06_120000_add_indexes_to_estoque_movimentacoes_table.php
+++ database/migrations/2026_02_06_120000_add_indexes_to_estoque_movimentacoes_table.php
@@ -8,6 +8,8 @@ return new class extends Migration
 {
     public function up(): void
     {
+        Schema::disableForeignKeyConstraints();
+
         Schema::table('estoque_movimentacoes', function (Blueprint $table) {
             $table->index(['id_variacao', 'data_movimentacao'], 'ix_em_var_data');
             $table->index(['id_variacao', 'tipo', 'data_movimentacao'], 'ix_em_var_tipo_data');
@@ -15,10 +17,14 @@ return new class extends Migration
             $table->index(['id_deposito_destino', 'data_movimentacao'], 'ix_em_dep_dest_data');
             $table->index(['data_movimentacao', 'id'], 'ix_em_data_id');
         });
+
+        Schema::enableForeignKeyConstraints();
     }
 
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
+
         Schema::table('estoque_movimentacoes', function (Blueprint $table) {
             $table->dropIndex('ix_em_var_data');
             $table->dropIndex('ix_em_var_tipo_data');
@@ -26,5 +32,7 @@ return new class extends Migration
             $table->dropIndex('ix_em_dep_dest_data');
             $table->dropIndex('ix_em_data_id');
         });
+
+        Schema::enableForeignKeyConstraints();
     }
 };

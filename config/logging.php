a2f7eb62d431ab24d9caa964e5f6bc3db96f8fc6
Albert Reis <albert.reis@darwinseguros.com>
2026-06-07 23:14:03 -0300

    chore: limpar warnings do deploy

--- config/logging.php
+++ config/logging.php
@@ -132,6 +132,11 @@ return [
             'handler' => NullHandler::class,
         ],
 
+        'testing_null' => [
+            'driver' => 'monolog',
+            'handler' => NullHandler::class,
+        ],
+
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
